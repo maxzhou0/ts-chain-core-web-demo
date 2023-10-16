aa5e23d4a6f511616c15df14dffb4d31c342ae93
maxzhou <maxzhou0@outlook.com>
2023-10-16 19:44:44 +0800

    clean code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { ChainCore, CurrentStateChainRef } from "ts-chain-core";
 
 type QueryType = string | Document | HTMLElement | EventTarget | null;
+
+const getQuery = (q: QueryType) => {
+  if (q === null || q === undefined) return document.body;
+  if (typeof q === "string") {
+    return document.querySelector(q) as HTMLElement;
+  } else return q as HTMLElement;
+};
 const $ = (q: QueryType) => {
-  const getQuery = (q: QueryType) => {
-    if (q === null || q === undefined) return document.body;
-    if (typeof q === "string") {
-      return document.querySelector(q) as HTMLElement;
-    } else return q as HTMLElement;
-  };
 
   return ChainCore(null)
     .extendRuntime<{ dom: HTMLElement }>()
@@ -66,40 +67,3 @@ $(document).ready(() => {
     });
 });
 
-class Example {
-  toChain() {
-    return ChainCore(this).extendFunctionsFromObject(Example);
-  }
-  add(a: number, b: number) {
-    console.log(a + b);
-  }
-  addOne(a: number) {
-    return (b: number) => {
-      console.log(a + b);
-    };
-  }
-  addOneV2(a:number){
-    return (b:number)=>{
-      return a+b;
-    }
-  }
-  addEnd(a: number, b: number) {
-    return a + b;
-  }
-}
-
-new Example()
-  .toChain()
-  .add(3, 5) //8
-  .add(4, 7) //11
-  .addOne(4)(5)(
-    //9
-    6
-  )(
-    //10
-    7
-  ) //11
-  .addOneV2(3)(4)
-  .addEnd(11, 10); //chain ended here
-  
-// .add(42,1)  //❌error, chain function break
